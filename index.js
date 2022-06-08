67d271cd7f9b19ba807711308379007b25ed9565
Zack Rogers <zack.rogers.t@gmail.com>
2022-06-08 16:56:44 -0600

    added failsafe for empty input

--- index.js
+++ index.js
@@ -6,22 +6,29 @@ function addMovie(event) {
     let inputField = document.querySelector("input");
     let movie = document.createElement("li");
     let movieTitle = document.createElement("span");
-    movieTitle.textContent = inputField.value;
-    movieTitle.addEventListener("click", crossOffMovie)
-    movie.appendChild(movieTitle);
 
-    let deleteBtn = document.createElement("button");
-    deleteBtn.textContent = "X";
-    deleteBtn.addEventListener("click", deleteMovie);
-    movie.appendChild(deleteBtn);
-
-    document.querySelector("ul").appendChild(movie);
-    inputField.value = "";
+    if (inputField.value === "" || inputField.value === " ") {
+        message.textContent = `"${inputField.value}" is not a valid entry`;
+        revealMessage()
+    } else {
+        movieTitle.textContent = inputField.value;
+        movieTitle.addEventListener("click", crossOffMovie)
+        movie.appendChild(movieTitle);
+    
+        let deleteBtn = document.createElement("button");
+        deleteBtn.textContent = "X";
+        deleteBtn.addEventListener("click", deleteMovie);
+        movie.appendChild(deleteBtn);
+    
+        document.querySelector("ul").appendChild(movie);
+        inputField.value = "";
+    }
 }
 
 const deleteMovie = event => {
     event.target.parentNode.remove();
-    message.textContent = `${event.target.parentElement.childNodes[0].textContent} Deleted`;
+    // message.textContent = `${event.target.parentNode.childNodes[0].textContent} Deleted`;
+    message.textContent = `"${event.target.parentNode.firstChild.textContent}" deleted`;
     revealMessage()
 }
 
