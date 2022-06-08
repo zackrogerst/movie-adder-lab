afff8d5b5a41a567603fa3b14df7cd9f9f15914b
Zack Rogers <zack.rogers.t@gmail.com>
2022-06-08 15:08:24 -0600

    added toggle

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
+
 function addMovie(event) {
     event.preventDefault();
     let inputField = document.querySelector("input");
     let movie = document.createElement("li");
     let movieTitle = document.createElement("span");
     movieTitle.textContent = inputField.value;
+    movieTitle.addEventListener("click", crossOffMovie)
     movie.appendChild(movieTitle);
-
     let deleteBtn = document.createElement("button");
     deleteBtn.textContent = "X";
     deleteBtn.addEventListener("click", deleteMovie);
     movie.appendChild(deleteBtn);
-
     document.querySelector("ul").appendChild(movie);
     inputField.value = "";
 }
 
-function deleteMovie(event){
-    event.target.parentNode.remove();
-}
+const deleteMovie = event => event.target.parentNode.remove()
+
+const crossOffMovie = event => event.target.classList.toggle("checked");
 
 document.querySelector("form").addEventListener("submit", addMovie);
