6bbbd1bbfb9c969467c02728e26702ac7e9f9af3
Zack Rogers <zack.rogers.t@gmail.com>
2022-06-08 16:32:34 -0600

    added intermediate steps

--- index.js
+++ index.js
@@ -9,27 +9,40 @@ function addMovie(event) {
     movieTitle.textContent = inputField.value;
     movieTitle.addEventListener("click", crossOffMovie)
     movie.appendChild(movieTitle);
+
     let deleteBtn = document.createElement("button");
     deleteBtn.textContent = "X";
     deleteBtn.addEventListener("click", deleteMovie);
     movie.appendChild(deleteBtn);
+
     document.querySelector("ul").appendChild(movie);
     inputField.value = "";
 }
 
 const deleteMovie = event => {
     event.target.parentNode.remove();
-    message.textContent = "Movie Deleted!";
+    message.textContent = `${event.target.parentElement.childNodes[0].textContent} Deleted`;
+    revealMessage()
 }
 
 const crossOffMovie = event => {
     event.target.classList.toggle("checked");
     if (event.target.classList.contains("checked")) {
-        message.textContent = "Movie Watched!";
+        message.textContent = `Marked "${event.target.textContent}" as watched`;
     } else {
-        message.textContent = "Movie Added Back";
+        message.textContent = `Marked "${event.target.textContent}" as un-watched`;
     }
+
+    revealMessage()
 }
 
 document.querySelector("form").addEventListener("submit", addMovie);
 
+
+
+function revealMessage() {
+    message.classList.remove('hide');
+    setTimeout(() => {
+        message.classList.add('hide');
+    }, '1500')
+}
